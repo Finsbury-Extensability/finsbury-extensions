5b0b6f3a4c7f1eebcc88aa50262d1d0873c67ad3
Neil Johnson <neil.johnson@infor.com>
2020-09-16 10:45:49 +0100

    Add code done by Manilla

---
+++ src/main/groovy/LstPickersPL.groovy
@@ -0,0 +1,237 @@
+/**
+ * EXT420MI.LstPickersPL
+ * Extension api for MWS420MI.LstPickersPL
+ * Date	    Changed By  Description
+ * 20200820	JEACAR01    Add ZCND - Customer name depot
+ */
+public class LstPickersPL extends ExtendM3Transaction {
+  private final MIAPI mi;
+  private final DatabaseAPI database;
+  private final MICallerAPI miCaller;
+  private final LoggerAPI logger;
+  private final ProgramAPI program;
+
+  public LstPickersPL(MIAPI mi, DatabaseAPI database, MICallerAPI miCaller, LoggerAPI logger, ProgramAPI program) {
+    this.mi = mi;
+    this.database = database;
+    this.miCaller = miCaller;
+    this.logger = logger;
+    this.program = program;
+  }
+
+  public void main() {
+
+    def callback = {
+      Map <String, String> out ->
+
+        if (out.error != null) {
+          mi.error(out.errorMessage);
+          return;
+        }
+
+        mi.outData.put("CONO", out.get("CONO").trim());
+        mi.outData.put("WHLO", out.get("WHLO").trim());
+        mi.outData.put("DLIX", out.get("DLIX").trim());
+        mi.outData.put("PLSX", out.get("PLSX").trim());
+        mi.outData.put("PISS", out.get("PISS").trim());
+        mi.outData.put("SLTP", out.get("SLTP").trim());
+        mi.outData.put("PISE", out.get("PISE").trim());
+        mi.outData.put("PLTM", out.get("PLTM").trim());
+        mi.outData.put("SEEQ", out.get("SEEQ").trim());
+        mi.outData.put("ESPD", out.get("ESPD").trim());
+        mi.outData.put("ESPT", out.get("ESPT").trim());
+        mi.outData.put("TEAM", out.get("TEAM").trim());
+        mi.outData.put("PICK", out.get("PICK").trim());
+        mi.outData.put("PLRI", out.get("PLRI").trim());
+        mi.outData.put("WPIC", out.get("WPIC").trim());
+        mi.outData.put("ARLD", out.get("ARLD").trim());
+        mi.outData.put("ARLT", out.get("ARLT").trim());
+        mi.outData.put("ARLE", out.get("ARLE").trim());
+        mi.outData.put("ARLF", out.get("ARLF").trim());
+        mi.outData.put("NPLL", out.get("NPLL").trim());
+        mi.outData.put("NPLR", out.get("NPLR").trim());
+        mi.outData.put("NPLP", out.get("NPLP").trim());
+        mi.outData.put("CLOD", out.get("CLOD").trim());
+        mi.outData.put("CLOT", out.get("CLOT").trim());
+        mi.outData.put("ZDEV", out.get("DEVI").trim());
+        mi.outData.put("DEVW", out.get("DEVW").trim());
+        mi.outData.put("TWLO", out.get("TWLO").trim());
+        mi.outData.put("OLIX", out.get("OLIX").trim());
+        mi.outData.put("RGDT", out.get("RGDT").trim());
+        mi.outData.put("RGTM", out.get("RGTM").trim());
+        mi.outData.put("LMDT", out.get("LMDT").trim());
+        mi.outData.put("CHNO", out.get("CHNO").trim());
+        mi.outData.put("CHID", out.get("CHID").trim());
+        mi.outData.put("DSDT", out.get("DSDT").trim());
+        mi.outData.put("DSHM", out.get("DSHM").trim());
+        mi.outData.put("TRDT", out.get("TRDT").trim());
+        mi.outData.put("GRWE", out.get("GRWE").trim());
+        mi.outData.put("PGRS", out.get("PGRS").trim());
+        mi.outData.put("PIST", out.get("PIST").trim());
+        mi.outData.put("CUNO", out.get("CUNO").trim());
+        mi.outData.put("CUNM", out.get("CUNM").trim());
+        mi.outData.put("RORC", out.get("RORC").trim());
+        mi.outData.put("VLME", out.get("VLME").trim());
+        mi.outData.put("VOL3", out.get("VOL3").trim());
+        mi.outData.put("ROUT", out.get("ROUT").trim());
+        mi.outData.put("RODN", out.get("RODN").trim());
+        mi.outData.put("ZROD", out.get("RODS").trim());
+        mi.outData.put("CONN", out.get("CONN").trim());
+        mi.outData.put("LODO", out.get("LODO").trim());
+        mi.outData.put("MODL", out.get("MODL").trim());
+        mi.outData.put("MODF", out.get("MODF").trim());
+        mi.outData.put("ZLPI", out.get("LPIC").trim());
+        mi.outData.put("ZWPI", out.get("WTPI").trim());
+        mi.outData.put("ZVPI", out.get("VTPI").trim());
+        mi.outData.put("ZLPA", out.get("LPAC").trim());
+        mi.outData.put("ZWPA", out.get("WTPA").trim());
+        mi.outData.put("ZVPA", out.get("VTPA").trim());
+        mi.outData.put("PRIO", out.get("PRIO").trim());
+        mi.outData.put("RIDN", out.get("RIDN").trim());
+        String zrdn = "";
+        String zcnd = "";
+        if (out.get("RORC").toInteger() == 3) {
+          DBAction query = database.table("MHDISH")
+            .index("00")
+            .selection("OQCONO","OQINOU","OQDLIX","OQRIDN").build();
+          DBContainer MHDISH = query.getContainer();
+          MHDISH.set("OQCONO", out.get("CONO").trim().toInteger());
+          MHDISH.set("OQINOU", 1);
+          MHDISH.set("OQDLIX", out.get("DLIX").trim().toInteger());
+          if(query.read(MHDISH)){
+            zrdn = MHDISH.get("OQRIDN");
+            zcnd = getDeliveryAddressName(zrdn, out);
+          }
+        }
+        mi.outData.put("ZRDN", zrdn);
+        mi.outData.put("ZCND", zcnd);
+        mi.write();
+    }
+
+    def params = [ "WHLO": mi.inData.get("WHLO").trim(),
+                   "FPIS": mi.inData.get("ZFPI").trim(),
+                   "TPIS": mi.inData.get("ZTPI").trim(),
+                   "FORC": mi.inData.get("ZFOR").trim(),
+                   "TORC": mi.inData.get("ZTOR").trim(),
+                   "FSEQ": mi.inData.get("ZFSE").trim(),
+                   "TSEQ": mi.inData.get("ZTSE").trim(),
+                   "SSEQ": mi.inData.get("ZSSE").trim(),
+                   "ZFTP": mi.inData.get("ZFTP").trim(),
+                   "ZTTP": mi.inData.get("ZTTP").trim(),
+                   "ZLST": mi.inData.get("ZLST").trim(),
+                   "TEAM": mi.inData.get("TEAM").trim(),
+                   "PICK": mi.inData.get("PICK").trim(),
+                   "DLIX": mi.inData.get("DLIX").trim(),
+                   "PLSX": mi.inData.get("PLSX").trim(),
+                   "PISE": mi.inData.get("PISE").trim(),
+                   "ZXPN": mi.inData.get("ZXPN").trim(),
+                   "ZXPL": mi.inData.get("ZXPL").trim(),
+                   "ROUT": mi.inData.get("ROUT").trim(),
+                   "RODN": mi.inData.get("RODN").trim(),
+                   "CONN": mi.inData.get("CONN").trim(),
+                   "PLRI": mi.inData.get("PLRI").trim(),
+                   "FDDT": mi.inData.get("ZFDD").trim(),
+                   "FDHM": mi.inData.get("ZFDH").trim(),
+                   "TDDT": mi.inData.get("ZTDD").trim(),
+                   "TDHM": mi.inData.get("ZTDH").trim(),
+                   "CONA": mi.inData.get("CONA").trim(),
+                   "FPAS": mi.inData.get("ZFPA").trim(),
+                   "TPAS": mi.inData.get("ZTPA").trim(),
+                   "ZPIS": mi.inData.get("ZPIS").trim(),
+                   "ZORC": mi.inData.get("ZORC").trim(),
+                   "ZPAS": mi.inData.get("ZPAS").trim(),
+                   "ZISE": mi.inData.get("ZISE").trim(),
+                   "CLPI": mi.inData.get("ZCPI").trim(),
+                   "CLPA": mi.inData.get("ZCPA").trim(),
+                   "CWV1": mi.inData.get("ZCW1").trim(),
+                   "CWV2": mi.inData.get("ZCW2").trim(),
+                   "XPI0": mi.inData.get("ZPI0").trim(),
+                   "XPA0": mi.inData.get("ZPA0").trim(),
+                   "FLPI": mi.inData.get("ZFLI").trim(),
+                   "TLPI": mi.inData.get("ZTLI").trim(),
+                   "FWPI": mi.inData.get("ZFWI").trim(),
+                   "TWPI": mi.inData.get("ZTWI").trim(),
+                   "FVPI": mi.inData.get("ZFVI").trim(),
+                   "TVPI": mi.inData.get("ZTVI").trim(),
+                   "FLPA": mi.inData.get("ZFLA").trim(),
+                   "TLPA": mi.inData.get("ZTLA").trim(),
+                   "FWPA": mi.inData.get("ZFWA").trim(),
+                   "TWPA": mi.inData.get("ZTWA").trim(),
+                   "FVPA": mi.inData.get("ZFVA").trim(),
+                   "TVPA": mi.inData.get("ZTVA").trim(),
+                   "MLPI": mi.inData.get("ZMLI").trim(),
+                   "MLPA": mi.inData.get("ZMLA").trim(),
+                   "RIDN": mi.inData.get("RIDN").trim()
+    ];
+    //.error("${params}");
+    miCaller.call("MWS420MI", "LstPickersPL", params, callback);
+  }
+
+  /**
+   * Retrieve delivery address name
+   * @param
+   * @return deliveryName
+   */
+  String getDeliveryAddressName(String orderNo, Map<String, String> params) {
+    String deliveryAddress = "";
+    DBAction query = database.table("OOHEAD")
+      .index("00")
+      .selection("OAORNO","OACUNO","OAADID").build();
+    DBContainer OOHEAD = query.getContainer();
+    OOHEAD.set("OACONO", program.LDAZD.get("CONO"));
+    OOHEAD.set("OAORNO", orderNo);
+    if(query.read(OOHEAD)){
+      int iter = 0;
+      Closure<?> OOADRECallback = {
+        DBContainer data->
+          if (iter > 1) {
+            return;
+          }
+          iter = iter + 1;
+          deliveryAddress = data.get("ODCUNM");
+          logger.debug("XtendM3Debug_LstPickersPL: ${orderNo} OOADRE ${deliveryAddress}");
+      }
+
+      query = database.table("OOADRE")
+        .index("00")
+        .selection("ODCUNM").build();
+      DBContainer OOADRE = query.getContainer();
+      OOADRE.set("ODCONO", OOHEAD.get("OACONO"));
+      OOADRE.set("ODORNO", OOHEAD.get("OAORNO"));
+      OOADRE.set("ODADRT", 1);
+
+      if(query.readAll(OOADRE, 3, OOADRECallback)){
+        return deliveryAddress;
+      }
+
+      query = database.table("OCUSAD")
+        .index("00")
+        .selection("OPCUNM").build();
+      DBContainer OCUSAD = query.getContainer();
+      OCUSAD.set("OPCONO", OOHEAD.get("OACONO"));
+      OCUSAD.set("OPCUNO", OOHEAD.get("OACUNO"));
+      OCUSAD.set("OPADRT", 1);
+      OCUSAD.set("OPADID", OOHEAD.get("OAADID"));
+      if(query.read(OCUSAD)){
+        deliveryAddress = OCUSAD.get("OPCUNM");
+        logger.debug("XtendM3Debug_LstPickersPL: ${orderNo} OCUSAD ${deliveryAddress}");
+        return deliveryAddress;
+      }
+
+      logger.debug("XtendM3Debug_LstPickersPL: ${orderNo} CONO=${OOHEAD.get('OACONO')} LDAZD=${params['CONO'].toInteger()} CUNO=${OOHEAD.get('OACUNO')} ${params['CUNO']}");
+      if (OOHEAD.get("OACONO") != params["CONO"].toInteger() || OOHEAD.get("OACUNO") != params["CUNO"]) {
+        query = database.table("OCUSMA")
+          .index("00")
+          .selection("OKCUNM").build();
+        DBContainer OCUSMA = query.getContainer();
+        OCUSMA.set("OKCONO", OOHEAD.get("OACONO"));
+        OCUSMA.set("OKCUNO", OOHEAD.get("OKCUNO"));
+        if(query.read(OCUSMA)){
+          deliveryAddress = OCUSMA.get("OKCUNM");
+          return deliveryAddress;
+        }
+      }
+    }
+    return deliveryAddress;
+  }
+}
---
+++ src/main/groovy/SelPickDetail.groovy
@@ -0,0 +1,245 @@
+/**
+ * EXT422MI.SelPickDetail
+ * Extension api for MWS422MI.SelPickDetail
+ * Date	    Changed By  Description
+ * 20200820	JEACAR01    Add ZMBD - Minimum best before days
+ */
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+public class SelPickDetail extends ExtendM3Transaction {
+  private final MIAPI mi;
+  private final DatabaseAPI database;
+  private final MICallerAPI miCaller;
+  private final LoggerAPI logger;
+  private final ProgramAPI program;
+
+  public SelPickDetail(MIAPI mi, DatabaseAPI database, MICallerAPI miCaller, LoggerAPI logger, ProgramAPI program) {
+    this.mi = mi;
+    this.database = database;
+    this.miCaller = miCaller;
+    this.logger = logger;
+    this.program = program;
+  }
+
+  public void main() {
+
+
+    def callback = {
+      Map <String, String> out ->
+
+        if (out.error != null) {
+          mi.error(out.errorMessage);
+          return;
+        }
+
+        int minBestBeforeDate = 0;
+        minBestBeforeDate = getMinBestBeforeDate(out);
+
+        mi.outData.put("CONO", out.get("CONO").trim());
+        mi.outData.put("WHLO", out.get("WHLO").trim());
+        mi.outData.put("ITNO", out.get("ITNO").trim());
+        mi.outData.put("WHSL", out.get("WHSL").trim());
+        mi.outData.put("BANO", out.get("BANO").trim());
+        mi.outData.put("CAMU", out.get("CAMU").trim());
+        mi.outData.put("REPN", out.get("REPN").trim());
+        mi.outData.put("TTYP", out.get("TTYP").trim());
+        mi.outData.put("RIDN", out.get("RIDN").trim());
+        mi.outData.put("RIDO", out.get("RIDO").trim());
+        mi.outData.put("RIDI", out.get("RIDI").trim());
+        mi.outData.put("PLSX", out.get("PLSX").trim());
+        mi.outData.put("RIDL", out.get("RIDL").trim());
+        mi.outData.put("STAT", out.get("STAT").trim());
+        mi.outData.put("SLTP", out.get("SLTP").trim());
+        mi.outData.put("PISE", out.get("PISE").trim());
+        mi.outData.put("ALQT", out.get("ALQT").trim());
+        mi.outData.put("SORT", out.get("SORT").trim());
+        mi.outData.put("TRFL", out.get("TRFL").trim());
+        mi.outData.put("MAAL", out.get("MAAL").trim());
+        mi.outData.put("RFTX", out.get("RFTX").trim());
+        mi.outData.put("PGNM", out.get("PGNM").trim());
+        mi.outData.put("PLPR", out.get("PLPR").trim());
+        mi.outData.put("PAQT", out.get("PAQT").trim());
+        mi.outData.put("CAWE", out.get("CAWE").trim());
+        mi.outData.put("FLOC", out.get("FLOC").trim());
+        mi.outData.put("ZDEV", out.get("DEV").trim());
+        mi.outData.put("STCD", out.get("STCD").trim());
+        mi.outData.put("SOFT", out.get("SOFT").trim());
+        mi.outData.put("TRQT", out.get("TRQT").trim());
+        mi.outData.put("TWSL", out.get("TWSL").trim());
+        mi.outData.put("ZTWL", out.get("TWS2").trim());
+        mi.outData.put("OEND", out.get("OEND").trim());
+        mi.outData.put("PLRI", out.get("PLRI").trim());
+        mi.outData.put("TRQA", out.get("TRQA").trim());
+        mi.outData.put("ALQA", out.get("ALQA").trim());
+        mi.outData.put("ALQN", out.get("ALQN").trim());
+        mi.outData.put("PAQA", out.get("PAQA").trim());
+        mi.outData.put("CNNR", out.get("CNNR").trim());
+        mi.outData.put("LSQN", out.get("LSQN").trim());
+        mi.outData.put("SSEQ", out.get("SSEQ").trim());
+        mi.outData.put("PLRN", out.get("PLRN").trim());
+        mi.outData.put("RGDT", out.get("RGDT").trim());
+        mi.outData.put("RGTM", out.get("RGTM").trim());
+        mi.outData.put("LMDT", out.get("LMDT").trim());
+        mi.outData.put("CHNO", out.get("CHNO").trim());
+        mi.outData.put("CHID", out.get("CHID").trim());
+        mi.outData.put("RIDX", out.get("RIDX").trim());
+        mi.outData.put("ITDS", out.get("ITDS").trim());
+        mi.outData.put("CONA", out.get("CONA").trim());
+        mi.outData.put("UNMS", out.get("UNMS").trim());
+        mi.outData.put("DCCD", out.get("DCCD").trim());
+        mi.outData.put("DPLO", out.get("DPLO").trim());
+        mi.outData.put("DDLO", out.get("DDLO").trim());
+        mi.outData.put("CUNO", out.get("CUNO").trim());
+        mi.outData.put("CUNM", out.get("CUNM").trim());
+        mi.outData.put("BACD", out.get("BACD").trim());
+        mi.outData.put("INDI", out.get("INDI").trim());
+        mi.outData.put("COMG", out.get("COMG").trim());
+        mi.outData.put("LOBL", out.get("LOBL").trim());
+        mi.outData.put("LODO", out.get("LODO").trim());
+        mi.outData.put("ACTI", out.get("ACTI").trim());
+        mi.outData.put("SCRQ", out.get("SCRQ").trim());
+        mi.outData.put("GENE", out.get("GENE").trim());
+        mi.outData.put("ZLON", out.get("LGON").trim());
+        mi.outData.put("AUCZ", out.get("AUCZ").trim());
+        mi.outData.put("ORRN", out.get("ORRN").trim());
+        mi.outData.put("ZWPI", out.get("WTPI").trim());
+        mi.outData.put("ZVPI", out.get("VTPI").trim());
+        mi.outData.put("ZWPA", out.get("WTPA").trim());
+        mi.outData.put("ZVPA", out.get("VTPA").trim());
+        mi.outData.put("PRIO", out.get("PRIO").trim());
+        mi.outData.put("CWUN", out.get("CWUN").trim());
+        mi.outData.put("PANR", out.get("PANR").trim());
+        mi.outData.put("MULS", out.get("MULS").trim());
+        mi.outData.put("SULS", out.get("SULS").trim());
+        mi.outData.put("SEEQ", out.get("SEEQ").trim());
+        mi.outData.put("DSDT", out.get("DSDT").trim());
+        mi.outData.put("DSHM", out.get("DSHM").trim());
+        mi.outData.put("CONN", out.get("CONN").trim());
+        mi.outData.put("ROUT", out.get("ROUT").trim());
+        mi.outData.put("RODN", out.get("RODN").trim());
+        mi.outData.put("MODL", out.get("MODL").trim());
+        mi.outData.put("MODF", out.get("MODF").trim());
+        mi.outData.put("RORC", out.get("RORC").trim());
+        mi.outData.put("ISEG", out.get("ISEG").trim());
+        mi.outData.put("ABFC", out.get("ABFC").trim());
+        mi.outData.put("ALUN", out.get("ALUN").trim());
+        mi.outData.put("GRWE", out.get("GRWE").trim());
+        mi.outData.put("NEWE", out.get("NEWE").trim());
+        mi.outData.put("VOL3", out.get("VOL3").trim());
+        mi.outData.put("FCU1", out.get("FCU1").trim());
+        mi.outData.put("FRAG", out.get("FRAG").trim());
+        mi.outData.put("ILEN", out.get("ILEN").trim());
+        mi.outData.put("IWID", out.get("IWID").trim());
+        mi.outData.put("IHEI", out.get("IHEI").trim());
+        mi.outData.put("DIM1", out.get("DIM1").trim());
+        mi.outData.put("DIM2", out.get("DIM2").trim());
+        mi.outData.put("DIM3", out.get("DIM3").trim());
+        mi.outData.put("RSVL", out.get("RSVL").trim());
+        mi.outData.put("RSVB", out.get("RSVB").trim());
+        mi.outData.put("RSVC", out.get("RSVC").trim());
+        if (minBestBeforeDate == 0) {
+          mi.outData.put("ZMBD", "");
+        } else {
+          mi.outData.put("ZMBD", minBestBeforeDate.toString());
+        }
+        mi.write();
+    }
+
+
+    def params = [ "DLIX": mi.inData.get("DLIX").trim(),
+                   "PLSX": mi.inData.get("PLSX").trim(),
+                   "PANR": mi.inData.get("PANR").trim(),
+                   "SSCC": mi.inData.get("SSCC").trim(),
+                   "ITNO": mi.inData.get("ITNO").trim(),
+                   "WHSL": mi.inData.get("WHSL").trim(),
+                   "BANO": mi.inData.get("BANO").trim(),
+                   "CAMU": mi.inData.get("CAMU").trim(),
+                   "PLRN": mi.inData.get("PLRN").trim(),
+                   "ISTS": mi.inData.get("ZITS").trim(),
+                   "ISTH": mi.inData.get("ZISH").trim(),
+                   "IRPK": mi.inData.get("ZIRP").trim(),
+                   "CWV1": mi.inData.get("ZCW1").trim(),
+                   "CWV2": mi.inData.get("ZCW2").trim(),
+                   "TL40": mi.inData.get("ZL40").trim(),
+                   "TL50": mi.inData.get("ZL50").trim(),
+                   "TL60": mi.inData.get("ZL60").trim(),
+                   "CONP": mi.inData.get("ZCOP").trim(),
+    ];
+
+    miCaller.call("MWS422MI", "SelPickDetail", params, callback);
+  }
+
+  /**
+   * Retrieve minimum best before date
+   * @param MWS422MI.SelPickDetail
+   * @return date yyyyMMdd
+   */
+  int getMinBestBeforeDate(Map<String, String> params) {
+    int bestDate = 0;
+    int cono = program.LDAZD.get("CONO");
+    String ridn =  params.get("RIDN").trim();
+    int ridl = params.get("RIDL").trim().toInteger();
+    int ridx = params.get("RIDX").trim().toInteger();
+    int ttyp = params.get("TTYP").trim().toInteger();
+
+    logger.debug("XtendM3Debug_SelPickDetail: CONO ${cono} RIDN ${ridn} RIDL ${ridl} RIDX ${ridx}");
+    if (ttyp == 31) {
+      DBAction query = database.table("OOLINE")
+        .index("00")
+        .selection("OBCONO","OBORNO","OBPONR","OBPOSX","OBITNO","OBCUNO","OBDWDZ").build();
+      DBContainer OOLINE = query.getContainer();
+      OOLINE.set("OBCONO", cono);
+      OOLINE.set("OBORNO", ridn);
+      OOLINE.set("OBPONR", ridl);
+      OOLINE.set("OBPOSX", ridx);
+
+      if(query.read(OOLINE)){
+        query = database.table("OCUSIT")
+          .index("00")
+          .selection("ORADCU").build();
+        DBContainer OCUSIT = query.getContainer();
+        OCUSIT.set("ORCONO", OOLINE.getInt("OBCONO"));
+        OCUSIT.set("ORCUNO", OOLINE.getString("OBCUNO"));
+        OCUSIT.set("ORITNO", OOLINE.getString("OBITNO"));
+        if(query.read(OCUSIT)){
+          LocalDate date = LocalDate.parse(OOLINE.getInt("OBDWDZ").toString(), DateTimeFormatter.ofPattern("yyyyMMdd"));
+          date = date.plusDays(OCUSIT.getInt("ORADCU"));
+          bestDate = date.format(DateTimeFormatter.ofPattern("YYYYMMdd")).toInteger();
+
+          logger.debug("XtendM3Debug_SelPickDetail: OCUSIT ${bestDate}");
+        }
+      }
+    } else if (ttyp == 41 || ttyp == 51 || ttyp == 92) {
+      DBAction query = database.table("MGLINE")
+        .index("00")
+        .selection("MRCONO","MRTRNR","MRPONR","MRPOSX","MRWHLO","MRITNO","MRTRDT").build();
+      DBContainer MGLINE = query.getContainer();
+      MGLINE.set("MRCONO", cono);
+      MGLINE.set("MRTRNR", ridn);
+      MGLINE.set("MRPONR", ridl);
+      MGLINE.set("MRPOSX", ridx);
+
+      if(query.read(MGLINE)){
+        query = database.table("MITBAL")
+          .index("00")
+          .selection("MBCONO","MBWHLO","MBITNO","MBREOP").build();
+        DBContainer MITBAL = query.getContainer();
+        MITBAL.set("MBCONO", MGLINE.getInt("MRCONO"));
+        MITBAL.set("MBWHLO", MGLINE.getString("MRWHLO"));
+        MITBAL.set("MBITNO", MGLINE.getString("MRITNO"));
+        if(query.read(MITBAL)){
+          LocalDate date = LocalDate.parse(MGLINE.getInt("MRTRDT").toString(), DateTimeFormatter.ofPattern("yyyyMMdd"));
+          date = date.plusDays(MITBAL.getDouble("MBREOP").toInteger());
+
+          bestDate = date.format(DateTimeFormatter.ofPattern("YYYYMMdd")).toInteger();
+
+          logger.debug("XtendM3Debug_SelPickDetail: MITBAL ${bestDate}");
+        }
+      }
+    }
+
+    return bestDate;
+  }
+}
